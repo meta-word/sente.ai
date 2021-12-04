a4fb602905d27f721adfc64bdac9138d4d1870c7
Will Budd <wbudd@users.noreply.github.com>
2021-12-04 19:22:17 +0900

    Add frontend project files (Vue3/Vite/TypeScript/Three.js)

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<!-- SPDX-License-Identifier: MIT  -->
+<!-- Copyright © 2021 MetaWord Inc -->
+
+<script setup lang="ts">
+</script>
+
+<template>
+</template>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*color: #2c3e50;*/
+}
+</style>
---
+++ frontend/src/main.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2021 MetaWord Inc
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('body')
